8939339e6764d81d093b675f33d3c3297e7cff2a
bavly-ashraf <bavly.ashraf2023@gmail.com>
2023-11-04 17:16:53 +0200

    editig vite.config.ts

--- src/Pages/Rank.tsx
+++ src/Pages/Rank.tsx
@@ -23,7 +23,7 @@ const Rank = () => {
             </div>
             <div className="row justify-content-center m-5">
                 {/* using conditional rendering to render an svg according to user rank */}
-                <img className='w-25' src={rank >= 90? "../src/assets/Images/gold-medal-svgrepo-com.svg": rank<50? "../src/assets/Images/football-field-svgrepo-com.svg": rank >= 50 && rank< 75? "../src/assets/Images/bronze-medal-svgrepo-com.svg" : "../src/assets/Images/silver-medal-svgrepo-com.svg"} alt='rank status' />
+                <img className='w-25' src={rank >= 90? "./assets/Images/gold-medal-svgrepo-com.svg": rank<50? "./assets/Images/football-field-svgrepo-com.svg": rank >= 50 && rank< 75? "./assets/Images/bronze-medal-svgrepo-com.svg" : "./assets/Images/silver-medal-svgrepo-com.svg"} alt='rank status' />
             </div>
             <div className="row">
                 {/* link to get back to words path to try again */}
