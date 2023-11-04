fe58774149f3ac670725a59cce806e41fb18ff81
bavly-ashraf <bavly.ashraf2023@gmail.com>
2023-11-04 16:21:28 +0200

    editig vite.config.ts

--- src/App.tsx
+++ src/App.tsx
@@ -9,7 +9,7 @@ function App() {
     <div className='container-fluid my-2'>
       <div className="row">
         {/* Clickable Logo that redirects to Home Page */}
-        <Link to='/' className='col-2 m-auto'><img src='./src/assets/Images/logo.svg' alt='Logo' /></Link>
+        <Link to='/' className='col-2 m-auto'><img src='assets/Images/logo.svg' alt='Logo' /></Link>
       </div>
     </div>
         {/* the router component that contains all routes */}
